75eac8d7a779eea90cfad62d507895aa40febf32
曾春辉 <nikolaszengchunhui@hotmail.com>
2018-07-04 19:56:02 +0800

    1

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,7 +2,7 @@
  * @Author: nikolas.zengchunhui 
  * @Date: 2018-07-01 13:39:22 
  * @Last Modified by: nikolas.zengchunhui
- * @Last Modified time: 2018-07-04 19:44:26
+ * @Last Modified time: 2018-07-04 19:53:24
  */
 import jsonp from 'common/js/jsonp'
 import { commonParams, options } from './config';
@@ -47,29 +47,29 @@ export function getDiscList() {
 
 
 
-export function getDiscList() {
-    const url = "/api/getDiscList";
+// export function getDiscList() {
+//     const url = "/api/getDiscList";
 
-    const data = Object.assign({}, commonParams, {
-        platform: "yqq",
-        hostUin: 0,
-        sin: 0,
-        ein: 29,
-        sortId: 5,
-        needNewCode: 0,
-        categoryId: 10000000,
-        rnd: Math.random(),
-        format: "json"
-    });
-    return axios
-        .get(url, {
-            params: data
-        })
-        .then(res => {
-            return Promise.resolve(res.data);
-        })
-        .catch(e => {
-            console.log(e);
-        });
-}
+//     const data = Object.assign({}, commonParams, {
+//         platform: "yqq",
+//         hostUin: 0,
+//         sin: 0,
+//         ein: 29,
+//         sortId: 5,
+//         needNewCode: 0,
+//         categoryId: 10000000,
+//         rnd: Math.random(),
+//         format: "json"
+//     });
+//     return axios
+//         .get(url, {
+//             params: data
+//         })
+//         .then(res => {
+//             return Promise.resolve(res.data);
+//         })
+//         .catch(e => {
+//             console.log(e);
+//         });
+// }
 
